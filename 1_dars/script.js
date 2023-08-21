53145bfc9e01f080b9d14ef3607d9f8ce7a2c068
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-08-21 15:57:51 +0500

    1-dars & 2-dars

---
+++ 1_dars/script.js
@@ -0,0 +1,122 @@
+// console.log('Hello world');
+// console.log('JavaScript 1-dars');
+
+// comment //; /* some comment */
+
+// Salomlashish
+// console.log('Hello World');
+
+// - semicolon (;) line finish
+// console.log('Hello world');
+// console.log('1-dars');
+
+// - Errors tell you what's wrong with your code in most cases
+// console.log('Hello world');
+
+// let ism = prompt('Ismingizni kiriting: ', 'Anvar');
+// console.log(ism);
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// const ism = 'Jack';
+
+// console.log(ism);
+// console.log(ism);
+// console.log(ism);
+// console.log(ism);
+// console.log(ism);
+
+// let n1 = 10;
+// let n2 = 10;
+// let n3 = 10;
+// let n1 = n2 = n3 = 10; // shorthand
+
+// let sum = n1 + n2 + n3;
+// console.log(sum);
+
+// string
+// '', "", ``
+// let name = 'Jack';
+// let name2 = 'Jack';
+// let name3 = `Jack`;
+
+// console.log(name, name2, name3);
+// console.log(typeof name);
+
+// number
+// let age = 20;
+// console.log(age);
+// console.log(typeof age);
+
+// boolean
+// let is_adult = true;
+// console.log(is_adult);
+// console.log(typeof is_adult);
+
+// undefined
+// let name;
+// console.log(name);
+// console.log(typeof name);
+
+// null
+// let name = null;
+
+// name = 1;
+// console.log(name);
+// console.log(typeof name);
+
+// symbol
+// let id = symbol
+
+// bigInt
+// let number = 11111111232;
+// console.log(number);
+
+// ---------------------------------------- NOTES ----------------------------------------
+// cd -> change directory
+// cd .. -> directoriyani bitta orqaga qaytarib beradi
+// clear -> terminalni tozalab beradi
+
+// console.log() -> Javascriptda yozgan kodlarimizni natijasini terminalda kuzatib borish uchun ishlatsak bo'ladi
+// Single Thread -> bir vaqtda faqat bitta amal bajara oladi.
+// comment - ctrl + / -> //, /* */ -> izoh yozib ketish uchun
+// - semicolon (;) line finish
+// ctrl + bectick -> terminal toggle
+// - Errors tell you what's wrong with your code in most cases
+// - alert(), prompt(), confirm();
+// alert('Siz laptopni savatchaga qoshdingiz'); // ok degan button bo'ladi. Browserda Message chiqarish uchun, ya'ni buni foydalanuvchini ogohlantirish uchun ishlatsak bo'ladi
+// prompt(placeholder, dastlabki qiymat) -> ok va cancel buttonlari bo'ladi. ok ni bossak formani submit qiladi. cancel bossak atmen qiladi va null javob qaytaradi. input field bo'ladi.
+// confirm() -> savol so'raydi. ok buttonni bossak true qaytaradi, cancelni bossak false qaytaradi
+//  =; ==; ===
+// = -> qiymat o'zlashtirish
+// == -> taqqoslash uchun
+// === -> qattiy taqqoslash uchun
+
+// var, let, const
+// var -> update, re-declare, function scope
+// let -> update, block scope
+// const -> no update, block scope
+
+// let n1 = 10;
+// let n2 = 10;
+// let n3 = 10;
+// let n1 = n2 = n3 = 10; // shorthand
+
+// Ma'lumot turlari -> xotirada joylashishi va qanday ma'lumot olishiga qarab ikki turga bo'linadi
+
+// 1. Primitive: string, number, boolean, undefined, null, symbol, bigInt
+// - callstackda joylashadi, faqat o'ziga bitta qiymat oladi
+// 2. Non-primitive: Object(array, obj, function)
+// - callstackda o'ziga id saqlaydi, heapda qiymati turadi. callstackdagi id orqali heapdagi manzildagi qiymatini olib beradi.
+// o'ziga birdan ortiq ma'lumot saqlay oladi
+
+// typeof() -> tipini tekshirish uchun
+
+// - Keyboard shortcut
+// 		- Ctrl+Shift+P, F1 -> Show Command Pallete
+// 		- Ctrl+P -> Quick Open, Go to File...
+// 		- Ctrl+Shift+N -> New Windwo/instance
+// 		- Ctrl+, -> User Settings
+// 		- Ctrl+S -> save
+// 		- Ctrl+K  -> Keyboard Shortcuts
---
+++ 2_dars/script.js
@@ -0,0 +1,111 @@
+// Type Conversion
+
+// let name = 'Jack';
+// let age = '10546';
+// console.log(typeof age);
+// // age = +age;
+// // age = Number(age);
+// // age = parseInt(age)
+// // age = parseFloat(age)
+// // console.log(age);
+// console.log(typeof age);
+
+// let age = 10; // '10'
+// console.log(age);
+// age = String(age); // Built-in function
+// age = age.toString(); // method
+
+// console.log(age);
+
+// let n1 = '10';
+// let n2 = '20';
+// let sum = +n1 + +n2;
+
+// console.log(sum);
+
+// Operators: + - * / % () = ++ -- **
+
+// +, -
+// let n1 = 5;
+// let n2 = 3;
+
+// let result = (n1 % n2) * (10 / 20);
+// console.log(result);
+
+// let n = 1;
+
+// n++ vs ++n
+// n = n + 1;
+// n += 1;
+// n++;
+
+// n--;
+// n = n - 1
+// n -= 1
+// n--
+// console.log(n);
+
+// let name = 'Jack';
+// let age = 20;
+
+// if (age == 20) {
+//   age++;
+// }
+
+// console.log(age);
+
+// **
+// let n1 = 8;
+// let result = n1 ** (1 / 3);
+
+// console.log(result);
+
+// Comparision: == === <> <= >=
+// let user_age = 31;
+// let n1 = 10;
+// let n2 = 30;
+// console.log(user_age >= n2);
+
+// Logical Operations: || && !
+// let n1 = 10;
+// let n2 = 30;
+// console.log(n1 > 9 || n1 < 8);
+// console.log(n1 > 9 && n1 < 8);
+
+// let user_age = 18;
+// let access_age = 18;
+
+// if (user_age < access_age) {
+//   console.log('Access denied');
+// } else {
+//   console.log('Welcome to Our website!');
+// }
+
+// let userName = 'jack001';
+// let userPassword = '1234';
+
+// if (userName === 'jack001' && userPassword === '1234') {
+//   console.log('Webcome back to our website!');
+
+// } else {
+//   if (userName !== 'jack001') {
+//     console.log('username da xatolik bor');
+//   }
+//   if (userPassword !== '1234') {
+//     console.log('userpassword da xatolik bor');
+//   }
+// }
+
+let userName = 'jack001';
+let userPassword = '1234';
+
+console.log(userPassword.length);
+// if(userName.length)
+
+// ------------------- NOTES -------------------------
+// Type Conversion -> ma'lumotlarni biror turdan boshqa turga o'tkazib olish
+// Operators: + - * / % () = ++ -- **
+// Comparision: == === <> <= >=   -> true/false qiymatlarini qaytaradi -> taqqoslash uchun ishlatiladi
+// Logical Operations: || && !
+// || -> yoki -> birinchi true bo'lgan qiymat izlaydi va usha joyda to'xtatadi true qiymatni qaytarib beradi, biror tomon true bo'lmasa false qaytaradi
+// && -> va -> bu birinchi false bo'lgan qiymatni izlaydi, topsa usha joyda to'xtaydi false qiymatni qaytarib beradi. BUnday qiymat bo'lmasa true qaytaradi
